ada57fad4d4642f11077f306daf894d5928cfecc
Vinicius de Borba <vinicius.borba@hotmail.com>
2025-02-12 00:39:31 -0300

    fix: updated pg password in init_db.sh

--- scripts/init_db.sh
+++ scripts/init_db.sh
@@ -15,7 +15,7 @@ DB_PORT="${DB_PORT:=5432}"
 SUPERUSER="${SUPERUSER:=postgres}"
 SUPERUSER_PWD="${SUPERUSER_PWD:=postgres}"
 APP_USER="${APP_USER:=app}"
-APP_USER_PWD="${APP_USER_PWD:=secret}"
+APP_USER_PWD="${APP_USER_PWD:=postgres}"
 APP_DB_NAME="${APP_DB_NAME:=newsletter}"
 
 # Allow to skip Docker if a dockerized Postgres database is already running
