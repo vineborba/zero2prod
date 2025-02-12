beb0ae3797f7e4a2008deaccef30852975aa8388
Vinicius de Borba <vinicius.borba@hotmail.com>
2025-02-12 20:55:04 -0300

    feat: implemented add subscription endpoint
    - refactored tests to use a db connection

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,17 @@
 use std::net::TcpListener;
 
+use sqlx::PgPool;
 use zero2prod::configuration::get_configuration;
 use zero2prod::startup::run;
 
 #[actix_web::main]
 async fn main() -> Result<(), std::io::Error> {
     let configuration = get_configuration().expect("Failed to read configuration.");
+    let db_pool = PgPool::connect(&configuration.database.connection_string())
+        .await
+        .expect("Failed to connect to Postgres.");
 
     let address = format!("127.0.0.1:{}", configuration.application_port);
     let listener = TcpListener::bind(address)?;
-    run(listener)?.await
+    run(listener, db_pool)?.await
 }
