9593ce2739a17852611f1d41a7656cb42e80708a
Vinicius de Borba <vinicius.borba@hotmail.com>
2025-02-12 00:36:53 -0300

    fix: updated pg password in init_db.sh

--- scripts/init_db.sh
+++ scripts/init_db.sh
@@ -13,7 +13,7 @@ fi
 # Check if a custom parameter has been set, otherwise use default values
 DB_PORT="${DB_PORT:=5432}"
 SUPERUSER="${SUPERUSER:=postgres}"
-SUPERUSER_PWD="${SUPERUSER_PWD:=password}"
+SUPERUSER_PWD="${SUPERUSER_PWD:=postgres}"
 APP_USER="${APP_USER:=app}"
 APP_USER_PWD="${APP_USER_PWD:=secret}"
 APP_DB_NAME="${APP_DB_NAME:=newsletter}"
