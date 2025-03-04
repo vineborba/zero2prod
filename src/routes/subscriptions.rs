04e76dbc967324b4c812b91fca2ecc624ff0cba6
Vinicius de Borba <vinicius.borba@hotmail.com>
2025-03-04 11:49:21 -0300

    feat: implemented first version of newsletter publishing

--- src/routes/subscriptions.rs
+++ src/routes/subscriptions.rs
@@ -135,7 +135,7 @@ pub async fn send_confirmation_email(
     );
 
     email_client
-        .send_email(new_subscriber.email, "Welcome!", &html_body, &plain_body)
+        .send_email(&new_subscriber.email, "Welcome!", &html_body, &plain_body)
         .await
 }
 
@@ -160,7 +160,7 @@ pub async fn store_token(
     Ok(())
 }
 
-fn error_chain_fmt(
+pub fn error_chain_fmt(
     e: &impl std::error::Error,
     f: &mut std::fmt::Formatter<'_>,
 ) -> std::fmt::Result {
